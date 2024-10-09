ae9f3298a84c11b023b45818b7d1069b86a91263
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-10-08 23:03:55 -0300

    build: config yarn workspace

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import {sum} from '@alura/utils/math/sum';
+
 
 export default function HomeScreen() {
   return (
     <main>
       <h1>Home</h1>
+      <p>soma: {sum(2, 2)}</p>
     </main>
   );
 }
